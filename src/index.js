323319a8ad5f2a138a0bfb33419417a5b8cfc3d3
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2020-06-12 16:10:17 +0300

    commit 12.06 v1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // JS
 
 // export for others scripts to use
-// import $ from 'jquery';
-// window.$ = $;
+import $ from 'jquery';
+import jQuery from 'jquery';
+window.$ = $;
+window.jQuery = jQuery;
 // import { tns } from 'tiny-slider'
 
 // import 'tiny-slider/dist/min/tiny-slider';
@@ -11,7 +13,32 @@ import 'popper.js/dist/popper.min'
 import './js/index'
 
 
+$('select').change(function(){
+    let text = $(this).find('option:selected').text();
+    let $aux = $('<select/>').append($('<option/>').text(text));
+    $(this).after($aux);
+    $(this).width(function () {
+        return $aux.width()
+    });
+    $aux.remove()
+}).change();
 
+$('.form-search__input').on('focus blur', function (evt) {
+    $(this).parent().parent().toggleClass('search-open', evt.type === 'focus');
+});
+
+$('.form-search').click(function () {
+    $(this).parent().addClass('search-open-mobil')
+});
+
+jQuery(function($){
+    $(document).mouseup(function (e){
+        let div = $(".form-search");
+        if (!div.is(e.target) && div.has(e.target).length === 0 && !$('#form-search-mobil').is(e.target)) {
+            div.parent().removeClass('search-open-mobil');
+        }
+    });
+});
 
 // SCSS
 import './assets/scss/main.scss'
