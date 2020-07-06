6e0be09bbafb9cf1a46d6e1b933be3d397fab0c5
Alexandr Petrovich <apalion97@gmail.com>
2020-07-06 09:03:14 +0300

    commit dist page v1

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,10 +1,16 @@
+const webpack =  require('webpack');
 const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.conf');
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
   // BUILD config
   mode: 'production',
-  plugins: []
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
 });
 
 module.exports = new Promise((resolve, reject) => {
