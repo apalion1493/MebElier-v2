76af65a329807e5c248a4797512b085af51bf98c
Alexandr Petrovich <apalion97@gmail.com>
2020-07-27 16:35:44 +0300

    commit 27.07

--- src/index.js
+++ src/index.js
@@ -306,7 +306,21 @@ function slickNum() {
     slickNum()
 })()
 
-
+let mapTitle = document.createElement('div'); mapTitle.className = 'mapTitle';
+mapTitle.textContent = 'Для активации карты нажмите по ней';
+wrapMap.appendChild(mapTitle);
+wrapMap.onclick = function() {
+    this.children[0].removeAttribute('style');
+    mapTitle.parentElement.removeChild(mapTitle);
+};
+wrapMap.onmousemove = function(event) {
+    mapTitle.style.display = 'block';
+    if(event.offsetY > 10) mapTitle.style.top = event.offsetY + 20 + 'px';
+    if(event.offsetX > 10) mapTitle.style.left = event.offsetX + 20 + 'px';
+};
+wrapMap.onmouseleave = function() {
+    mapTitle.style.display = 'none';
+};
 
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,3 @@
-// let dropdownHeader = document.getElementById("myselectPhone");
-// dropdownHeader.addEventListener("DOMSubtreeModified", function () {
-//     // document.getElementById("myTextPhone").innerHTML = `<a href="tel:${dropdownHeader.dataset.value}">${dropdownHeader.dataset.value}</a>`;
-//     console.log(dropdownHeader.dataset.value)
-// });
-
-
 const slider = document.querySelectorAll('.product-image__block');
 let isDown = false;
 let startX;
